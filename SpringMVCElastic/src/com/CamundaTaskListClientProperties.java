d80d2da6d47c4f19966934ecc4c7935b5af4b17b
Vikash Kumar <vikash.kumar6@globallogic.com>
2026-05-15 09:59:33 +0530

    Added properties class

---
+++ SpringMVCElastic/src/com/CamundaTaskListClientProperties.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.time.Duration;
+
+import io.camunda.common.auth.Authentication;
+
+public class CamundaTaskListClientProperties {
+    public static final String CAMUNDA_FORMS_PREFIX = "camunda-forms:bpmn:";
+
+    public Authentication authentication;
+
+    public String taskListUrl;
+
+    public boolean defaultShouldReturnVariables;
+    public boolean defaultShouldLoadTruncatedVariables;
+    public boolean alwaysReconnect = false;
+    public Duration cookieExpiration = Duration.ofMinutes(3);
+}
